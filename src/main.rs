a1bb8962cf1ff7178ee31ce084a821a1d1ab2827
Fardin Khan <fardinkhanfahmi@gmail.com>
2026-07-04 00:51:39 +0600

    Cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-mod loop_numbers;
 mod vector_loop;
 mod while_loop;
 mod frequency_count;
 fn main() {
-    loop_numbers::run();
     vector_loop::run();
     while_loop::run();
     frequency_count::run();
