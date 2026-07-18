62b7305bd990a86cba0468ab3d19280e111652a1
Fardin Khan <fardinkhanfahmi@gmail.com>
2026-07-18 19:51:17 +0600

    Remaned the repository

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ mod vector_loop;
 mod while_loop;
 mod frequency_count;
 fn main() {
-    vector_loop::run();
-    while_loop::run();
-    frequency_count::run();
+    // vector_loop::run();
+    // while_loop::run();
+    // frequency_count::run();
+    println!("He");
 }
