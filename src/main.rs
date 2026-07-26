58f684185613055e1417e5a3ff9ff0b39201ef20
Fardin Khan <fardinkhanfahmi@gmail.com>
2026-07-27 03:17:57 +0600

    Add random number guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod frequency_count;
 mod vector_loop;
 mod while_loop;
+mod guessing_game;
 fn main() {
+    // loop_numbers::run();
     // vector_loop::run();
     // while_loop::run();
     // frequency_count::run();
-    println!("He");
+    guessing_game::run();
 }
