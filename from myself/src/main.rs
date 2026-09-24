43dd05d6dcea49bc5db4c2ca5f30b2a0ae882d65
Fardin Khan <fardinkhanfahmi@gmail.com>
2026-09-25 00:16:35 +0600

    print hello in rust

--- from myself/src/main.rs
+++ from myself/src/main.rs
@@ -1,11 +1,6 @@
-mod frequency_count;
-mod vector_loop;
+mod hello;
 mod while_loop;
-mod guessing_game;
 fn main() {
-    // loop_numbers::run();
-    // vector_loop::run();
-    // while_loop::run();
-    // frequency_count::run();
-    guessing_game::run();
+    hello::run();
+    while_loop::run();
 }
