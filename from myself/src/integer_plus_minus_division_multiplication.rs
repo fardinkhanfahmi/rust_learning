c76717b3e0f7d24e23cefb46909b4f56de08f7a0
Fardin Khan <fardinkhanfahmi@gmail.com>
2026-09-25 19:45:15 +0600

    learn mutable variable

---
+++ from myself/src/integer_plus_minus_division_multiplication.rs
@@ -0,0 +1,12 @@
+pub fn run()
+{
+    let mut a=100;
+    a=a+2;
+    println!("a={}",a);
+    a=a-2;
+    println!("a={}",a);
+    a*=a;
+    println!("a={}",a);
+    a/=a;
+    println!("a={}",a);
+}
--- from myself/src/main.rs
+++ from myself/src/main.rs
@@ -1,8 +1,10 @@
 mod hello;
 mod hello_new_line;
 mod integer_variable;
+mod integer_plus_minus_division_multiplication;
 fn main() {
     hello::run();
     hello_new_line::run();
     integer_variable::run();
+    integer_plus_minus_division_multiplication::run();
 }
