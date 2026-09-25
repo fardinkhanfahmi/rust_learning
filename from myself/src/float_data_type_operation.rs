4b802115414eb1126c397a0f8a5c65b72bf2f598
Fardin Khan <fardinkhanfahmi@gmail.com>
2026-09-25 20:07:47 +0600

    float data operations

---
+++ from myself/src/float_data_type_operation.rs
@@ -0,0 +1,6 @@
+pub fn run()
+{
+    let mut a=100.0;
+    a/=3.0;
+    println!("a={}",a);
+}
--- from myself/src/main.rs
+++ from myself/src/main.rs
@@ -2,9 +2,11 @@ mod hello;
 mod hello_new_line;
 mod integer_variable;
 mod integer_plus_minus_division_multiplication;
+mod float_data_type_operation;
 fn main() {
-    hello::run();
-    hello_new_line::run();
-    integer_variable::run();
-    integer_plus_minus_division_multiplication::run();
+    //hello::run();
+    //hello_new_line::run();
+    //integer_variable::run();
+    //integer_plus_minus_division_multiplication::run();
+    float_data_type_operation::run();
 }
